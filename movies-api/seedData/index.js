80d186c526e86a4e86d5f880401f7d3ecbacf39d
Kate O’Neill <kateoneill@Kates-MBP.wit.ie>
2022-04-26 10:35:54 +0100

    Topic 10 - Update Movies Data/Route

--- movies-api/seedData/index.js
+++ movies-api/seedData/index.js
@@ -4,6 +4,8 @@ import users from './users';
 
 // import Genre from './genres';
 import dotenv from 'dotenv';
+import movieModel from '../api/movies/movieModel';
+import movies from './movies.js';
 
 dotenv.config();
 
@@ -29,8 +31,21 @@ async function loadUsers() {
 //     console.error(`failed to Load genre Data: ${err}`);
 //   }
 // }
+// deletes all movies documents in collection and inserts test data
+export async function loadMovies() {
+  console.log('load seed data');
+  console.log(movies.length);
+  try {
+    await movieModel.deleteMany();
+    await movieModel.collection.insertMany(movies);
+    console.info(`${movies.length} Movies were successfully stored.`);
+  } catch (err) {
+    console.error(`failed to Load movie Data: ${err}`);
+  }
+}
 
 if (process.env.SEED_DB) {
   loadUsers();
   // loadGenres();
+  loadMovies();
 }
