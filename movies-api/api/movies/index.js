542ac9d77759eaf200527a395157024dacaaa6f5
Kate O’Neill <kateoneill@Kates-MacBook-Pro.local>
2022-05-02 16:13:55 +0100

    Topic 11 - Finished

--- movies-api/api/movies/index.js
+++ movies-api/api/movies/index.js
@@ -3,6 +3,7 @@ import { movies, movieReviews, movieDetails } from './moviesData';
 import uniqid from 'uniqid';
 import movieModel from './movieModel';
 import asyncHandler from 'express-async-handler';
+import { getUpcomingMovies } from '../tmdb-api';
 
 const router = express.Router(); 
 router.get('/', asyncHandler(async (req, res) => {
@@ -34,6 +35,11 @@ router.get('/:id/reviews', (req, res) => {
       });
   }
 });
+
+router.get('/tmdb/upcoming', asyncHandler( async(req, res) => {
+    const upcomingMovies = await getUpcomingMovies();
+    res.status(200).json(upcomingMovies);
+  }));
 
 router.post('/:id/reviews', (req, res) => {
   const id = parseInt(req.params.id);
